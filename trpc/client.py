21e91716617b1d4f1cbbb52d2c725231c7ea7223
tef <tef@twentygototen.org>
2018-12-13 05:38:09 +0000

    Client Objects

--- trpc/client.py
+++ trpc/client.py
@@ -7,82 +7,160 @@ from urllib.parse import urljoin, urlencode
 
 from . import objects
 
+class APIRequest:
+    def __init__(self, verb, url, data, cached):
+        self.verb = verb
+        self.url = url
+        self.data = data
+        self.cached = cached
+    
+class APIResponse:
+    def __init__(self, base_url, kind, apiVersion, metadata, fields):
+        self.base_url = base_url
+        self.kind = kind
+        self.apiVersion = apiVersion
+        self.metadata = metadata
+        self.fields = fields
+
+    def __repr__(self):
+        return self.kind
+
+    def open_link(self, name):
+        links = self.metadata['links']
+        if name not in links:
+            raise Exception(name)
+
+        url = self.metadata['urls'].get(name, name)
+
+        url = urljoin(self.base_url, url)
+
+        cached = self.metadata.get('embeds',{}).get(name)
+
+        return APIRequest('GET', url, None, cached)
+
+    def has_link(self, name):
+        if 'links' in self.metadata:
+            return name in self.metadata['links']
+
+    def has_form(self, name):
+        if 'forms' in self.metadata:
+            return name in self.metadata['forms']
+
+    def submit_form(self, name, args):
+        links = self.metadata['links']
+        forms = self.metadata['forms']
+        if name not in forms:
+            if name in links:
+                url = self.metadata['urls'].get(name, name)
+                url = urljoin(self.base_url, url)
+                return APIRequest('GET', url, None)
+            raise Exception(name)
+
+        url = self.metadata['urls'].get(name, name)
+        url = urljoin(self.base_url, url)
+        
+        arguments = {}
+        form_args = forms[name]
+        if form_args:
+            while args:
+                name, value = args.pop(0)
+                if name is None:
+                    name = form_args.pop(0)
+                    arguments[name] = value
+                else:
+                    arguments[name] = value
+                    form_args.remove(name)
+        else:
+            arguments = args
+
+        return APIRequest('POST', url, arguments, None)
+
+    def wrap(self, session):
+        if self.kind == 'Result':
+            return self.fields['value']
+        elif self.kind == 'Namespace':
+            return Namespace(self, session)
+        elif self.kind == 'Service':
+            return Service(self, session)
+        elif self.kind == 'Collection':
+            return Collection(self, session)
+        else:
+            return self
+
+class APIClient:
+    pass
+
+class Navigable(APIClient):
+    def __init__(self, response, session=None):
+        self._response = response
+        self._session = session
+
+    def _fetch(self, req):
+        if self._session:
+            response = self._session.fetch(req)
+            return response.wrap(self._session)
+        else:
+            return req
+    def __getattr__(self, name):
+        if self._response.has_link(name):
+            req = self._response.open_link(name)
+            return self._fetch(req)
+        if self._response.has_form(name):
+            def method(**args):
+                req = self._response.submit_form(name, args)
+                return self._fetch(req)
+            return method
+        raise Exception('no')
+
+class Namespace(Navigable):
+    pass
+
+class Service(Navigable):
+    pass
+
+class Collection(APIClient):
+    def __init__(self, response, session=None):
+        self._response = response
+        self._session = session
+
+    def _fetch(self, req):
+        if self._session:
+            response = self._session.fetch(req)
+            return response.wrap(self._session)
+        else:
+            return req
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def get(self, key):
+        pass
+
+    def create(self, **args):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def list(self, **args):
+        pass
+
+    def next(self):
+        return self.list()
+
+    def where(self, **args):
+        pass
+
+    def not_where(self, **args):
+        pass
+
 class Session:
     def __init__(self):
         pass
 
-    class APIRequest:
-        def __init__(self, verb, url, data, cached):
-            self.verb = verb
-            self.url = url
-            self.data = data
-            self.cached = cached
-        
-    class APIResponse:
-        def __init__(self, base_url, kind, apiVersion, metadata, fields):
-            self.base_url = base_url
-            self.kind = kind
-            self.apiVersion = apiVersion
-            self.metadata = metadata
-            self.fields = fields
-
-
-        def __repr__(self):
-            return self.kind
-
-        def open_link(self, name):
-            links = self.metadata['links']
-            if name not in links:
-                raise Exception(name)
-
-            url = self.metadata['urls'].get(name, name)
-
-            url = urljoin(self.base_url, url)
-
-            cached = self.metadata.get('embeds',{}).get(name)
-
-            return Session.APIRequest('GET', url, None, cached)
-
-        def has_link(self, name):
-            if 'links' in self.metadata:
-                return name in self.metadata['links']
-
-        def has_form(self, name):
-            if 'forms' in self.metadata:
-                return name in self.metadata['forms']
-
-        def submit_form(self, name, args):
-            links = self.metadata['links']
-            forms = self.metadata['forms']
-            if name not in forms:
-                if name in links:
-                    url = self.metadata['urls'].get(name, name)
-                    url = urljoin(self.base_url, url)
-                    return Session.APIRequest('GET', url, None)
-                raise Exception(name)
-
-            url = self.metadata['urls'].get(name, name)
-            url = urljoin(self.base_url, url)
-            
-            arguments = {}
-            form_args = forms[name]
-            if form_args:
-                while args:
-                    name, value = args.pop(0)
-                    if name is None:
-                        name = form_args.pop(0)
-                        arguments[name] = value
-                    else:
-                        arguments[name] = value
-                        form_args.remove(name)
-            else:
-                arguments = args
-
-            return Session.APIRequest('POST', url, arguments, None)
-
     def fetch(self, request):
         if isinstance(request, str):
-            request = self.APIRequest("GET", request, None, None)
+            request = APIRequest("GET", request, None, None)
 
         obj = request.cached
         if not obj:
@@ -109,37 +187,14 @@ class Session:
         apiVersion = obj.pop('apiVersion')
         metadata = obj.pop('metadata')
 
-        return self.APIResponse(base_url, kind, apiVersion, metadata, obj)
+        return APIResponse(base_url, kind, apiVersion, metadata, obj)
 
 
-class Client:
-    def __init__(self, session, response):
-        self.session = session
-        self.obj = response
 
-    def unwrap(self, obj):
-        if isinstance(obj, self.session.APIResponse):
-            if obj.kind == 'Result':
-                return obj.fields['value']
-            return Client(self.session, obj)
-        return obj
-
-    def __getattr__(self, name):
-        if self.obj.has_link(name):
-            req = self.obj.open_link(name)
-            obj = self.session.fetch(req)
-            return self.unwrap(obj)
-        if self.obj.has_form(name):
-            def method(**args):
-                req = self.obj.submit_form(name, args)
-                obj = self.session.fetch(req)
-                return self.unwrap(obj)
-            return method
-        raise Exception('no')
 
 def open(endpoint):
     session = Session()
     obj = session.fetch(endpoint)
-    return Client(session, obj)
+    return obj.wrap(session)
 
     
